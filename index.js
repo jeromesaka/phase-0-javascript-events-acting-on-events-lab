96a8af0cf98e2ea57772ace867b882546424a166
Ogwenya <emanuelogwenya@outlook.com>
2023-03-12 16:52:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+dodger = document.getElementById("dodger");
+
+const moveDodgerLeft = () => {
+	const leftNumbers = dodger.style.left.replace("px", "");
+	const left = parseInt(leftNumbers, 10);
+
+	if (left > 0) {
+		dodger.style.left = `${left - 1}px`;
+	}
+};
+
+const moveDodgerRight = () => {
+	const rightNumbers = dodger.style.left.replace("px", "");
+	const right = parseInt(rightNumbers, 10);
+
+	if (right > 0) {
+		dodger.style.left = `${right + 1}px`;
+	}
+};
